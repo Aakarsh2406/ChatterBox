e296639976e560b0857d4f427bdbe449e9098723
AakarshSingh2406 <46308570+Aakarsh2406@users.noreply.github.com>
2019-04-04 11:04:22 +0530

    Add files via upload
    Multiplication of matrix

---
+++ matrix_multiplication.py
@@ -0,0 +1,28 @@
+def dimension(A):
+    return len(A),len(A[0])
+
+
+A = [[21,44,5],
+     [3,77,9]]
+
+B = [[2,6],
+     [3,7],
+     [2,4]]
+
+r1,c1=dimension(A)
+r2,c2=dimension(B)
+
+if c1==r2:
+    result =[[0,0],
+             [0,0]]
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                result[i][j] += A[i][k] *B[k][j]
+
+else:
+    print('dimension not match ')
+
+
+for i in result:
+    print(i)
